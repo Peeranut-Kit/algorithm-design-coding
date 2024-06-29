0e67fbc770a844284da722483d1d3875a0fb7a6b
Peeranut-Kit <peeranut.kit.work@gmail.com>
2024-06-29 23:03:02 +0700

    Final version day_cover

--- problem/day_cover/day_cover.cpp
+++ problem/day_cover/day_cover.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bitset>
 using namespace std;
 
 // not minimum
-int day_cover_v1(vector< pair <int, int> > &staff_and_numfree, vector< vector <bool> > &availability, vector<bool> &covered_day_vector, int covered_day, int len, int staff_count, int n)
+int day_cover_v1(vector< pair <int, int> > &staff_and_numfree, vector< vector <bool> > &availability, vector<bool> &covered_day_vector, int covered_day, int len, int staff_count, int n, int m)
 {
     while (covered_day < n) {
-        if (len == n) return -1;
+        if (len == m) return -1;
         int staff_id = staff_and_numfree[len].second;
         bool used = false;
         for (size_t i=0; i<n; i++) {
@@ -18,7 +19,6 @@ int day_cover_v1(vector< pair <int, int> > &staff_and_numfree, vector< vector <b
             }
         }
         if (used) staff_count++;
-        // cout << staff_count << " " << len << endl;
         len++;
     }
     return staff_count;
@@ -48,6 +48,29 @@ int day_cover_v2(vector< vector <bool> > &availability, vector<bool> covered_day
     return min(exclude, include);
 }
 
+// use combination + std::bitset
+int day_cover_v3(vector< bitset<1000> > &availability, bitset<1000> covered_day_vector, int len, int m, int n)
+{
+    // All days are covered, end of this path.
+    if (covered_day_vector.count() == n) {
+        return 0;
+    }
+
+    // Iterate until last one but all days are not covered -> This path cannot be the candidate solution.
+    if (len == m) {
+        return m+1;
+    }
+
+    // exclude current staff (staff count is not incremented.)
+    int exclude = day_cover_v3(availability, covered_day_vector, len+1, m, n);
+
+    // include current staff (staff count++)
+    covered_day_vector |= availability[len];
+
+    int include = day_cover_v3(availability, covered_day_vector, len+1, m, n) + 1;
+    return min(exclude, include);
+}
+
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
@@ -55,8 +78,7 @@ int main(int argc, char const *argv[])
     int n, m;
     cin >> n >> m;
 
-    vector< pair <int, int> > staff_and_numfree;
-    vector< vector<bool> > availabilty(m);
+    vector< bitset<1000> > availabilty(m);
     for (int i = 0; i < m; i++)
     {
         int days_free;
@@ -68,36 +90,15 @@ int main(int argc, char const *argv[])
             return 0;
         }
 
-        staff_and_numfree.push_back(make_pair(days_free, i));
-        vector<bool> tmp(n);
-        availabilty[i] = tmp;
-
         for (int j = 0; j < days_free; j++)
         {
             int day;
             cin >> day;
-            availabilty[i][day - 1] = true;
+            availabilty[i].set(day - 1); // Using 0-based indexing
         }
     }
 
-    sort(staff_and_numfree.begin(), staff_and_numfree.end());
-    reverse(staff_and_numfree.begin(), staff_and_numfree.end());
-
-    /*for (int i = 0; i < m; i++)
-    {
-        cout << staff_and_numfree[i].first << " " << staff_and_numfree[i].second << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << availabilty[i][j] << " ";
-        }
-        cout << endl;
-    }*/
-
-    vector<bool> covered_day_vector(n);
-    cout << day_cover_v2(availabilty, covered_day_vector, 0, m);
+    bitset<1000>covered_day_vector;
+    cout << day_cover_v3(availabilty, covered_day_vector, 0, m, n);
     return 0;
 }
